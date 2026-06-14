467d7fc031d3892bf54ec1b4f4d862a15a542413
Nitin99732 <bharranitin@gmail.com>
2026-06-14 22:59:27 +0530

    create recursive_token_splitter and add all of the recursive text splitter function modified for tokens

--- Chunking/recursive_token_splitter.py
+++ Chunking/recursive_token_splitter.py
@@ -1,18 +1,21 @@
 from  langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 
-def get_text_splitter() -> RecursiveCharacterTextSplitter:
+def get_token_splitter() -> RecursiveCharacterTextSplitter:
 
     """
-    Create and return a RecursiveCharacterTextSplitter instance.
+    Create and return a token-aware
+    RecursiveCharacterTextSplitter.
 
-     The splitter recursively attempts to split text using:
+    The splitter recursively attempts to split text using:
         Paragraphs -> Lines -> Words -> Characters
+    
+    Chunk size and chunk overlap are measured in tokens.
 
     Chunk overlap helps preserve context between adjacent chunks.
     """
 
-    return RecursiveCharacterTextSplitter(
+    return RecursiveCharacterTextSplitter.from_tiktoken_encoder(
         chunk_size = 100, 
         chunk_overlap = 20
     )
@@ -22,14 +25,16 @@ def get_text_splitter() -> RecursiveCharacterTextSplitter:
 def chunking_with_split_text(text : str) -> list[str]:
 
     """
-    Split raw text into chunks
+    Split text into chunks 
+    based on token count.
 
     Input : str
 
     Output : list[str]
     """
 
-    splitter = get_text_splitter()
+
+    splitter = get_token_splitter()
 
     chunks = splitter.split_text(text)
 
@@ -40,15 +45,16 @@ def chunking_with_split_text(text : str) -> list[str]:
 def chunking_with_create_document(text : str) -> list[Document]:
 
     """
-    Split text into Langchain document objects 
-    and attach chunk numbers as metadata.
+    Split text into LangChain Document chunks
+    based on token count and attach chunk
+    numbers as metadata.
 
     Input : str
 
     Output : list[Document]
     """
-
-    splitter = get_text_splitter()
+    
+    splitter = get_token_splitter()
 
     chunks = splitter.create_documents([text])
 
@@ -64,14 +70,15 @@ def chunking_with_split_documents(documents : list[Document]) -> list[Document]:
 
     """
     Split LangChain Document objects into smaller 
-    Document chunks while preserving metadata.
+    Document chunks based on token count
+    while preserving metadata.
 
     Input : list[Document]
 
     Output : list[Document]
     """
 
-    splitter = get_text_splitter()
+    splitter = get_token_splitter()
 
     chunks = splitter.split_documents(documents)
 
@@ -82,14 +89,15 @@ def chunking_with_transform_documents(documents : list[Document]) -> list[Docume
 
     """
     Transform LangChain Document objects into smaller 
-    Document chunks while preserving metadata.
+    Document chunks based on token count
+    while preserving metadata.
 
     Input : list[Document]
 
     Output : list[Document]
     """
 
-    splitter = get_text_splitter()
+    splitter = get_token_splitter()
 
     chunks = splitter.transform_documents(documents)
 
